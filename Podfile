45e3c0cc9b17d88d3c08119d9824ff0a952eff47
Maxime Le Moine <lm.maxime@gmail.com>
2018-05-27 13:29:41 +0200

    First commit

---
+++ Podfile
@@ -0,0 +1,9 @@
+# Uncomment the next line to define a global platform for your project
+platform :osx, '10.10'
+
+target 'TestPodInstall' do
+  #use_frameworks!
+
+  pod 'RxSwift'
+
+end
---
+++ TestPodInstall/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TestPodInstall
+//
+//  Created by Maxime Le Moine on 27/05/2018.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
